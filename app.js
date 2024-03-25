3c3ed64f4140f0d89ff114d8ce679064d03c01c7
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-03-25 18:21:50 +0200

    sending responses

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const http = require("http");
-const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-})
-server.listen(3000)
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from node server</h1></body>')
+  res.write('</html>');
+
+});
+server.listen(3000);
