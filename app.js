ef2dd44b4dede602bdce1fb938f067511f32daa6
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-02 01:11:51 +0200

    passing incoming requests

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const route = require("./routes");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middlware");
   res.send(
-    '<html><form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form></html>'
+    '<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form></html>'
   );
 });
-app.use('/pr')
-app.use("/", (req, res, next) => {
-  console.log("next middleware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+
 });
+app.use("/", (req, res, next) => {});
 app.use(route);
 app.listen(3000);
