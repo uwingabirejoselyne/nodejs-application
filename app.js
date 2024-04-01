7323c574de072100737c3be1809c2f0b1136dc15
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-01 20:46:49 +0200

    handling different routes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const http = require("http");
 const route = require("./routes");
 const express = require("express");
 const app = express();
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
   console.log("In the middlware");
   res.send('<h1>Sending middleware!</h1>')
-  next()
 });
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
   console.log("next middleware");
 });
 app.use(route)
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
