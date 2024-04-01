bb75311e18b42cc60efd9a59794de73ce62df5ce
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-02 00:47:02 +0200

    body parser installation

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const route = require("./routes");
 const express = require("express");
 const app = express();
-app.use('/add-product',(req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In the middlware");
-  res.send('<h1>Sending middleware!</h1>')
+  res.send(
+    '<html><form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form></html>'
+  );
 });
-app.use('/',(req, res, next) => {
+app.use('/pr')
+app.use("/", (req, res, next) => {
   console.log("next middleware");
 });
-app.use(route)
+app.use(route);
 app.listen(3000);
