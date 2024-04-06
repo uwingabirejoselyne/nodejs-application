1482acee9040c3c9fd9391a5f5852bfca33269bb
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-06 08:29:30 +0200

    serving add-product page

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => { 
-    res.send(
-        '<html><form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form></html>'
-    );
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
+  console.log(req.body);
+  res.redirect("/");
 });
 
 module.exports = router;
