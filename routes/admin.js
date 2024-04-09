d1c3382a7cd7982a508c837c5bfde8e75a65fcc1
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-09 10:26:09 +0200

    rendering added title in the card

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const path = require("path");
-const rootDir = require('../util/path')
+const rootDir = require("../util/path");
 const router = express.Router();
-const product = []
+const products = [];
+const fs = require("fs")
 
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  // product.push({title: req.body.title})
-  // res.redirect("/");
-  res.sendFile(path.join(rootDir,"views","card.html"))
+  const title = req.body.title;
+  products.push({ title: title });
+  fs.readFile(path.join(rootDir, "views", "card.html"), "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Internal server error");
+    }
+    const renderedcard = data.replace("{{title}}", title);
+    res.send(renderedcard);
+  });
+  // res.sendFile(path.join(rootDir, "views", "card.html"));
+
 });
 
 // module.exports = router;
-exports.routes = router
-exports.products = product
+exports.routes = router;
+exports.products = products;
